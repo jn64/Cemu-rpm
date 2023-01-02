0d77cef3887353b69401865083bdbac5e7d1b528
Justin Koh <j@ustink.org>
2023-01-03 00:40:00 +0800

    Update tag-release.sh

--- tag-release.sh
+++ tag-release.sh
@@ -4,13 +4,23 @@
 
 readonly specfile='Cemu.spec'
 
-readonly version_release="$(rpmspec -q --qf '%{version}-%{release}\n' "${specfile}" | head -1)"
-readonly without_dist="${version_release%.fc*}"
+# shellcheck disable=SC2155
+readonly vr="$(rpmspec -q --qf '%{version}-%{release}\n' "${specfile}" | head -1)"
+readonly vr_nodist="${vr%.fc*}"
 
 # Normalises the following to - (hyphen-minus):
 #   space ~ ^ : ? *
-readonly normalised="$(sed -E -e 's@[ ~^:?*]@-@g' <<<"${without_dist}")"
+# shellcheck disable=SC2155
+readonly vr_nodist_norm="$(sed -E -e 's@[ ~^:?*]@-@g' <<<"${vr_nodist}")"
 
-printf 'Tagging HEAD as:  %s\n' "${normalised}"
-git tag "${normalised}" &&
-echo 'Use this for GitHub release title, filename etc.'
+printf '%-25s:  %s\n' \
+	'Version-Release (no dist)' "${vr_nodist}" \
+	'Normalised for git' "${vr_nodist_norm}"
+
+echo 'Tagging HEAD...'
+if git tag "${vr_nodist_norm}"; then
+	# Copy to clipboard
+	xclip -selection clipboard -rmlastnl <<<"${vr_nodist_norm}"
+	echo 'Copied to clipboard. Use it for GitHub release title and filenames.'
+	echo 'Remember to git push the tag too.'
+fi
